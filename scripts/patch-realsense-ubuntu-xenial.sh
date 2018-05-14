04f41d14e4951bbdfeea3f0d406934df4bedf5bd
Pierre-Luc St-Charles <stcharpl@crim.ca>
2018-05-14 22:40:36 +0000

    Jetson TX2 Spacely fixups

--- scripts/patch-realsense-ubuntu-xenial.sh
+++ scripts/patch-realsense-ubuntu-xenial.sh
@@ -29,34 +29,12 @@ require_package libssl-dev
 LINUX_BRANCH=$(uname -r)
 
 kernel_branch=$(choose_kernel_branch $LINUX_BRANCH)
-kernel_name="ubuntu-xenial-$kernel_branch"
+kernel_name="kernel-4.4"
 
 # Get the linux kernel and change into source tree
-[ ! -d ${kernel_name} ] && git clone -b $kernel_branch git://kernel.ubuntu.com/ubuntu/ubuntu-xenial.git --depth 1 ./${kernel_name}
+#[ ! -d ${kernel_name} ] && git clone -b $kernel_branch git://kernel.ubuntu.com/ubuntu/ubuntu-xenial.git --depth 1 ./${kernel_name}
+cp -R /usr/src/kernel/${kernel_name} ./${kernel_name}
 cd ${kernel_name}
-
-# Verify that there are no trailing changes., warn the user to make corrective action if needed
-if [ $(git status | grep 'modified:' | wc -l) -ne 0 ];
-then
-	echo -e "\e[36mThe kernel has modified files:\e[0m"
-	git status | grep 'modified:'
-	echo -e "\e[36mProceeding will reset all local kernel changes. Press 'n' within 10 seconds to abort the operation"
-	set +e
-	read -n 1 -t 10 -r -p "Do you want to proceed? [Y/n]" response
-	set -e
-	response=${response,,}    # tolower
-	if [[ $response =~ ^(n|N)$ ]]; 
-	then
-		echo -e "\e[41mScript has been aborted on user requiest. Please resolve the modified files are rerun\e[0m"
-		exit 1
-	else
-		echo -e "\e[0m"
-		echo -e "\e[32mUpdate the folder content with the latest from mainline branch\e[0m"
-		git fetch origin $kernel_branch --depth 1
-		printf "Resetting local changes in %s folder\n " ${kernel_name}
-		git reset --hard $kernel_branch
-	fi
-fi
 
 #Check if we need to apply patches or get reload stock drivers (Developers' option)
 [ "$#" -ne 0 -a "$1" == "reset" ] && reset_driver=1 || reset_driver=0
@@ -77,8 +55,8 @@ else
 fi
 
 # Copy configuration
-sudo cp /usr/src/linux-headers-$(uname -r)/.config .
-sudo cp /usr/src/linux-headers-$(uname -r)/Module.symvers .
+sudo cp /usr/src/kernel/${kernel_name}/.config .
+sudo cp /usr/src/kernel/${kernel_name}/Module.symvers .
 
 # Basic build for kernel modules
 #yes "" | make silentoldconfig modules_prepare
@@ -108,16 +86,23 @@ sudo make -j -C $KBASE M=$KBASE/drivers/media/v4l2-core modules
 
 # Copy the patched modules to a sane location
 sudo cp $KBASE/drivers/media/usb/uvc/uvcvideo.ko ~/$LINUX_BRANCH-uvcvideo.ko
-sudo cp $KBASE/drivers/iio/accel/hid-sensor-accel-3d.ko ~/$LINUX_BRANCH-hid-sensor-accel-3d.ko
-sudo cp $KBASE/drivers/iio/gyro/hid-sensor-gyro-3d.ko ~/$LINUX_BRANCH-hid-sensor-gyro-3d.ko
-sudo cp $KBASE/drivers/media/v4l2-core/videodev.ko ~/$LINUX_BRANCH-videodev.ko
+#sudo cp $KBASE/drivers/iio/accel/hid-sensor-accel-3d.ko ~/$LINUX_BRANCH-hid-sensor-accel-3d.ko
+#sudo cp $KBASE/drivers/iio/gyro/hid-sensor-gyro-3d.ko ~/$LINUX_BRANCH-hid-sensor-gyro-3d.ko
+#sudo cp $KBASE/drivers/media/v4l2-core/videodev.ko ~/$LINUX_BRANCH-videodev.ko
+mkdir -p ~/$LINUX_BRANCH
+sudo cp $KBASE/drivers/media/v4l2-core/*.ko ~/$LINUX_BRANCH/
+sudo cp $KBASE/drivers/media/usb/uvc/*.ko ~/$LINUX_BRANCH/
 
 echo -e "\e[32mPatched kernels modules were created successfully\n\e[0m"
 
 # Load the newly-built modules
-try_module_insert videodev				~/$LINUX_BRANCH-videodev.ko 			/lib/modules/`uname -r`/kernel/drivers/media/v4l2-core/videodev.ko
+#try_module_insert videodev				~/$LINUX_BRANCH-videodev.ko 			/lib/modules/`uname -r`/kernel/drivers/media/v4l2-core/videodev.ko
+try_module_insert videobuf2-core        ~/$LINUX_BRANCH/videobuf2-core.ko       /lib/modules/`uname -r`/kernel/drivers/media/v4l2-core/videobuf2-core.ko
+try_module_insert videobuf2-v4l2        ~/$LINUX_BRANCH/videobuf2-v4l2.ko       /lib/modules/`uname -r`/kernel/drivers/media/v4l2-core/videobuf2-v4l2.ko
+try_module_insert videobuf2-vmalloc     ~/$LINUX_BRANCH/videobuf2-vmalloc.ko     /lib/modules/`uname -r`/kernel/drivers/media/v4l2-core/videobuf2-vmalloc.ko
+try_module_insert videobuf2-memops      ~/$LINUX_BRANCH/videobuf2-memops.ko     /lib/modules/`uname -r`/kernel/drivers/media/v4l2-core/videobuf2-memops.ko
 try_module_insert uvcvideo				~/$LINUX_BRANCH-uvcvideo.ko 			/lib/modules/`uname -r`/kernel/drivers/media/usb/uvc/uvcvideo.ko
-try_module_insert hid_sensor_accel_3d 	~/$LINUX_BRANCH-hid-sensor-accel-3d.ko 	/lib/modules/`uname -r`/kernel/drivers/iio/accel/hid-sensor-accel-3d.ko
-try_module_insert hid_sensor_gyro_3d	~/$LINUX_BRANCH-hid-sensor-gyro-3d.ko 	/lib/modules/`uname -r`/kernel/drivers/iio/gyro/hid-sensor-gyro-3d.ko
+#try_module_insert hid_sensor_accel_3d 	~/$LINUX_BRANCH-hid-sensor-accel-3d.ko 	/lib/modules/`uname -r`/kernel/drivers/iio/accel/hid-sensor-accel-3d.ko
+#try_module_insert hid_sensor_gyro_3d	~/$LINUX_BRANCH-hid-sensor-gyro-3d.ko 	/lib/modules/`uname -r`/kernel/drivers/iio/gyro/hid-sensor-gyro-3d.ko
 
 echo -e "\e[92m\n\e[1mScript has completed. Please consult the installation guide for further instruction.\n\e[0m"
